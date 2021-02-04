c4baec4d9ab9e0d4b0cf2db6cc964a62e6ebdfcd
Long Do <longdo@Longs-MacBook-Air-2.local>
2021-02-04 15:16:45 +0700

    abstract class does not need to get prop

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -2,7 +2,4 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   Failure([List properties = const <dynamic>[]]);
-
-  @override
-  List<dynamic> get props => [];
 }
