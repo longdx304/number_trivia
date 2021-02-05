b57e77b6187b41c0f5ea0b0ed645e42dce9137c7
Long Do <longdo@Longs-MacBook-Air-2.local>
2021-02-05 16:00:10 +0700

    fix import

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,4 +1,4 @@
-import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,12 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:number_trivia/core/platform/network_info.dart';
-import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
-import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 
 import '../../../../core/error/failure.dart';
+import '../../../../core/platform/network_info.dart';
 import '../../domain/entities/number_trivia.dart';
 import '../../domain/repositories/number_trivia_repository.dart';
+import '../datasources/number_trivia_local_data_source.dart';
+import '../datasources/number_trivia_remote_data_source.dart';
 
 class NumberTriviaRepositoryImpl extends NumberTriviaRepository {
   final NumberTriviaRemoteDataSource remoteDataSource;
