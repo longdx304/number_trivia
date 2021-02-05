d97cb2a8e939ed4e6913dfe7723af5d856d59e47
Long Do <longdo@Longs-MacBook-Air-2.local>
2021-02-05 11:30:22 +0700

    implemented fromJson method for model

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../domain/entities/number_trivia.dart';
 
 class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({
     @required int number,
     @required String text,
   }) : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      number: (json['number'] as num).toInt(),
+      text: json['text'],
+    );
+  }
 }
